cd47fa56ac7517bc9bcc52de1628f98bd4ac472d
Emina Prlja <eprlja1@etf.unsa.ba>
2015-05-18 09:43:17 +0200

    MVVM Klase - pocetak implementacije modela

---
+++ Kodiranje/Bike Shop Đir/Model/StavkaKorpe.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bike_Shop_Đir.Model
+{
+    class StavkaKorpe
+    {
+        static int redniBroj = 0;
+
+        private int kolicina;
+
+        public int Kolicina
+        {
+            get { return kolicina; }
+            set { kolicina = value; }
+        }
+
+        Usluga tipUsluge;
+
+        private float trenutnaCijena;
+
+        public float TrenutnaCijena
+        {
+            get { return tipUsluge.CijenaUsluge * kolicina; }
+            set { trenutnaCijena = value; }
+        }
+
+     /*   enum TipUsluge { KupovinaBicikla, Servis, Tura }
+
+        public TipUsluge tipUsluge { get; set; }*/
+
+
+        public StavkaKorpe()
+        {
+
+        }
+
+
+
+        
+        
+
+
+    }
+}
