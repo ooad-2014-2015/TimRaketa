26ea182d12bd38c6715f5d7fe02475ecde43e77d
Emina Prlja <eprlja1@etf.unsa.ba>
2015-05-18 10:09:53 +0200

    Klase, pocetak implementacije modela

---
+++ Kodiranje MVVM/Bike Shop Đir/Model/Biciklo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bike_Shop_Đir.Model
+{
+    abstract class Biciklo : Usluga
+    {
+        public string idBicikla { get; set; }
+
+        enum TipBicikla { Brdsko, Cestovno, Downhill, Bmx, Rekreacijsko }
+        
+        public TipBicikla tipBicikla { get; set; }
+
+
+        public abstract float izracunajCijenu();
+    }
+}
