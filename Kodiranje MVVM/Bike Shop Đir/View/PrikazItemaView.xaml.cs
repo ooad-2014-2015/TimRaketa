26b3b629ed8d677353c3a6c278c23ce00ddf7afc
Emin Šehić <emin.sehic13@gmail.com>
2015-05-20 17:19:39 +0200

    User control, forma za bicikle

---
+++ Kodiranje MVVM/Bike Shop Đir/View/PrikazItemaView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bike_Shop_Đir.View
+{
+    /// <summary>
+    /// Interaction logic for PrikazItemaView.xaml
+    /// </summary>
+    public partial class PrikazItemaView : UserControl
+    {
+        public PrikazItemaView()
+        {
+            InitializeComponent();
+        }
+    }
+}
